e504457fc560c2b4bfb902148ce389977254cb52
Christian Asch <casch@cenat.ac.cr>
2024-03-30 08:56:43 -0600

    added lsp support for elixir

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -5,7 +5,7 @@ require("mason-lspconfig").setup(
   })
 
 
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 local keymap = vim.keymap
 local lsp = vim.lsp
 local diag = vim.diagnostic
@@ -37,11 +37,11 @@ end
 local lspconfig = require("lspconfig")
 vim.g.coq_settings = { auto_start = "shut-up" }
 local coq = require("coq")
-lspconfig.lua_ls.setup{
+lspconfig.lua_ls.setup {
   coq.lsp_ensure_capabilities({ on_attach = on_attach }),
   on_init = function(client)
     local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
       client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
         Lua = {
           runtime = {
@@ -68,7 +68,8 @@ lspconfig.lua_ls.setup{
     return true
   end
 }
-lspconfig.clangd.setup( coq.lsp_ensure_capabilities({ on_attach = on_attach }))
-lspconfig.cmake.setup{ coq.lsp_ensure_capabilities({ on_attach = on_attach })}
-lspconfig.pylsp.setup{ coq.lsp_ensure_capabilities({ on_attach = on_attach })}
-lspconfig.texlab.setup{ coq.lsp_ensure_capabilities({ on_attach = on_attach })}
+lspconfig.clangd.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
+lspconfig.cmake.setup { coq.lsp_ensure_capabilities({ on_attach = on_attach }) }
+lspconfig.pylsp.setup { coq.lsp_ensure_capabilities({ on_attach = on_attach }) }
+lspconfig.texlab.setup { coq.lsp_ensure_capabilities({ on_attach = on_attach }) }
+lspconfig.elixirls.setup { coq.lsp_ensure_capabilities({ on_attach = on_attach }) }
